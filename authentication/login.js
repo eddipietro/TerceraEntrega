15ad3cde2eabcbe321a092ef0b00d962a5e838a3
eddipietro <94018665+eddipietro@users.noreply.github.com>
2023-03-14 23:54:23 -0300

    Tercera Entrega incompleta

---
+++ authentication/login.js
@@ -0,0 +1,41 @@
+const passport = require("passport");
+
+const LocalStrategy = require("passport-local").Strategy;
+const UserModel = require("../dataBase/models/user");
+
+const { isValidPassword } = require("../utils/utils");
+
+const log4js = require("../utils/logs");
+
+const loggerArchiveError = log4js.getLogger("errorArchive");
+
+const login = () => {
+
+
+  passport.use(
+    "login",
+    new LocalStrategy(
+      {
+        //Configuración para obtener todo el req.
+        passReqToCallback: true,
+      },
+      async (req, username, password, done) => {
+        try {
+          const user = await UserModel.findOne({ username });
+          if (!user) {
+            return done(null, false);
+          }
+          if (!isValidPassword(user.password, password)) {
+            return done(null, false);
+          }
+          return done(null, user);
+        } catch (err) {
+          loggerArchiveError.error(err);
+          done(err);
+        }
+      }
+    )
+  );
+};
+
+module.exports = login;
